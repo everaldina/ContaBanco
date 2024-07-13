639dc8a2108a4fad249db44c4bebec87f2b4af37
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-07-13 19:54:05 -0300

    docs: adicionando documentacao java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,4 +1,14 @@
-// desafio: https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe
+
+/**
+ * Desafio para prática de sintaxe em Java
+ * Um terminal de conta bancária que solicita ao usuário o número da conta, agência, nome do cliente e saldo da conta.
+ * Desafio para o bootcamp da Santander Coders | Java - Digital Innovation One
+ * link: https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe
+ * 
+ * @author: everaldina
+ * @version: 1.0.0
+ * @since: 2022-07-13
+ */
 
 import java.text.DecimalFormat;
 import java.util.InputMismatchException;
@@ -48,6 +58,15 @@ public class ContaTerminal {
         scanner.close();
     }
 
+    /**
+     * Cria a mensagem de saída com os dados da conta
+     * @param numero Número da conta
+     * @param agencia Agência da conta
+     * @param nomeCliente Nome do cliente
+     * @param saldo Saldo da conta
+     * 
+     * @return Mensagem de saída com o saldo formatado com duas casas decimais e o nome do cliente capitalizado
+     */
     public static String criarMensagemSaida(int numero, String agencia, String nomeCliente, float saldo) {
         String mensagem = "Olá " + capitalizar(nomeCliente) + ", obrigado por criar uma conta em nosso banco, ";
         mensagem += "sua agência é " + agencia;
@@ -57,6 +76,11 @@ public class ContaTerminal {
         return mensagem;
     }
 
+    /**
+     * Capitaliza a primeira letra de cada palavra de um texto
+     * @param texto Texto a ser capitalizado
+     * @return Texto com a primeira letra de cada palavra capitalizada
+     */
     public static String capitalizar(String texto) {
         String textoSeparado[] = texto.toLowerCase().split(" ");
         for (int i = 0; i < textoSeparado.length; i++) {
